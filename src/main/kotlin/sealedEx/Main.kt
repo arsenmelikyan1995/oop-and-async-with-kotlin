3b15a5c8b5fa53d884dc56cfed30b26b4044aa54
Arsen_Melikyan <arsen.melikyan@ukg.com>
2023-06-07 15:18:12 +0400

    added sealed package

---
+++ src/main/kotlin/sealedEx/Main.kt
@@ -0,0 +1,18 @@
+package sealedEx
+
+fun render(state: UiState) = when (state) {
+    is UiState.Error -> println("Error")
+    is UiState.Loaded -> println("Loaded: ${state.title}")
+    UiState.Loading -> println("Loading")
+}
+
+fun main() {
+    var state: UiState = UiState.Loading
+    render(state)
+
+    state = UiState.Loaded("Kotlin", "Is cool!")
+    render(state)
+
+    state = UiState.Error(IllegalStateException())
+    render(state)
+}
